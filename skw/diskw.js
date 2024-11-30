e892049e257d27ae2278accedfad1410ddc7afa2
yogik23 <61941105+yogik23@users.noreply.github.com>
2024-12-01 01:00:51 +0700

    Update diskw.js

--- skw/diskw.js
+++ skw/diskw.js
@@ -16,10 +16,10 @@ function displayskw() {
   console.log(chalk.hex('#800080')(" ╔══════════════════════════════════════════════╗"));
   console.log(chalk.hex('#800080')(" ║      Fitur Autobot by SKW AIRDROP HUNTER     ║"));
   console.log(chalk.hex('#800080')(" ║══════════════════════════════════════════════║"));
-  console.log(chalk.hex('#00FFFF')(" ║ ➤ 1️⃣  Multi Akun                    ║"));
+  console.log(chalk.hex('#00FFFF')(" ║ ➤ 1️⃣  Multi Akun                            ║"));
   console.log(chalk.hex('#00FFFF')(" ║ ➤ 2️⃣  Auto Login                            ║"));
-  console.log(chalk.hex('#00FFFF')(" ║ ➤ 3️⃣  Jangan Jual                ║"));
-  console.log(chalk.hex('#00FFFF')(" ║ ➤ 4️⃣  JANGAN DIJUAL                          ║"));
+  console.log(chalk.hex('#00FFFF')(" ║ ➤ 3️⃣  Jangan Jual                           ║"));
+  console.log(chalk.hex('#00FFFF')(" ║ ➤ 4️⃣  JANGAN DIJUAL                         ║"));
   console.log(chalk.hex('#800080')(" ╚══════════════════════════════════════════════╝"));
   console.log(chalk.hex('#74BBFB')("   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"));
   console.log('\n\n');
